710571a40be9026b546e80aee63953dc4df8c007
Nicholas Langley <nlangley@unr.edu>
2024-12-01 17:40:08 -0800

    removed ignored files

--- adventofcode2024/target/debug/adventofcode2024.d
+++
@@ -1,1 +0,0 @@
-/home/nicholas/adventofcode2024/target/debug/adventofcode2024: /home/nicholas/adventofcode2024/src/day01.rs /home/nicholas/adventofcode2024/src/main.rs
--- adventofcode2024/target/debug/deps/adventofcode2024-aba0bb67b87bcc32.d
+++
@@ -1,6 +0,0 @@
-/home/nicholas/adventofcode2024/target/debug/deps/adventofcode2024-aba0bb67b87bcc32: src/main.rs src/day01.rs
-
-/home/nicholas/adventofcode2024/target/debug/deps/adventofcode2024-aba0bb67b87bcc32.d: src/main.rs src/day01.rs
-
-src/main.rs:
-src/day01.rs:
